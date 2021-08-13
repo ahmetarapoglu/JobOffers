d479b3082c60ff4d9c1bafd1084723c94a34af50
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2021-08-13 16:10:42 +0300

    JobOffers

---
+++ Models/Job.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobOffers.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        [DisplayName("Job Name")]
+        public string JobTitle { get; set; }
+        [DisplayName("job Description")]
+        public string JobContent { get; set; }
+        
+        [DisplayName("Job Image")]
+        public string JobImage { get; set; }
+
+        [DisplayName("Job Type")]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }  
+    }
+}
